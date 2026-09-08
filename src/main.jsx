cd83e28daa97cce5879f0c401dffe96b9ec6b8a6
Kamana Agrawal <kamanaagrawal833@gmail.com>
2026-09-09 01:50:20 +0530

    fix: wrap router in ThemeProvider and other context providers to fix useTheme error

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { router } from './router'
 
+import { ThemeProvider } from './context/ThemeProvider'
+import { MotionPreferenceProvider } from './context/MotionPreferenceContext'
+import { AuthProvider } from './context/AuthContext'
+import { WorkspaceProvider } from './context/WorkspaceContext'
+
 import './styles/reset.css'
 import './styles/tokens.css'
 import './styles/typography.css'
@@ -12,7 +17,15 @@ import './styles/global.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <ThemeProvider>
+        <MotionPreferenceProvider>
+          <AuthProvider>
+            <WorkspaceProvider>
+              <RouterProvider router={router} />
+            </WorkspaceProvider>
+          </AuthProvider>
+        </MotionPreferenceProvider>
+      </ThemeProvider>
     </HelmetProvider>
   </StrictMode>,
 )
